9465d135f45aa9108ce6848122071d13ce4076a1
Uhmpasterig <805luca805@gmail.com>
2024-03-17 19:19:13 +0100

    chore: Update eslint max warnings
    Updated the lint script to use "--max-warnings 0" instead of "--max-warning 0" for consistency and to correctly set the maximum number of allowed warnings. Keeping the linting rules consistent is essential for maintaining code quality and readability.

---
+++ packages/eslint-config/nest.js
@@ -0,0 +1,21 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['eslint:recommended', 'prettier'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
