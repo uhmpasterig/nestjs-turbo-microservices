fb64dfa92faf0b2f2eb360efb86eb43b7a035ce6
Uhmpasterig <805luca805@gmail.com>
2024-03-17 16:36:40 +0100

    refactor: Update tsconfig for better TypeScript configuration
    - Updated tsconfig to improve TypeScript configuration settings
    - Set isolatedModules to true for better encapsulation
    - Added resolveJsonModule for JSON module resolution
    - Switched module to NodeNext for better module support

---
+++ packages/database/src/client.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+
+declare const global: {
+  prisma: PrismaClient;
+};
+
+export const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
+export * from '@prisma/client';
